ed0ae528fbc00d540ecd7f69040b43619e870b33
akashkumars883 <ak706908@gmail.com>
2026-07-14 17:01:00 +0530

    Fix permissions issue in start.sh for Docker deployment

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ php artisan db:seed --force
 php artisan app:seed-permissions
 php artisan cache:clear
 php artisan config:clear
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 apache2-foreground
